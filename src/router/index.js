bcb37952b345737f6e56dc744b844bb0a234d176
Lisbeth <13511707@qq.com>
2021-11-17 12:26:55 +0800

    修改路径

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,84 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import store from "../store";
 
-Vue.use(VueRouter)
+window.store = store;
+
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Index",
+    component: () => import("../views/Index/template.vue"),
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/login",
+    name: "Login",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () => import("../views/Login/template.vue"),
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: () => import("../views/Register/template.vue"),
+  },
+  {
+    path: "/detail/:blogId",
+    name: "Detail",
+    meta: { requiresAuth: true }, /* 我也不想加，但是如果不这样的话，通过接口没法取到res */
+    component: () => import("../views/Detail/template.vue"),
+  },
+  {
+    path: "/edit/:blogId",
+    name: "Edit",
+    meta: { requiresAuth: true },
+    component: () => import("../views/Edit/template.vue"),
+  },
+  {
+    path: "/create",
+    name: "Create",
+    meta: { requiresAuth: true },
+    component: () => import("../views/Create/template.vue"),
+  },
+  {
+    path: "/user/:userId",
+    name: "User",
+    component: () => import("../views/User/template.vue"),
+  },
+  {
+    path: "/my",
+    name: "My",
+    meta: { requiresAuth: true },
+    component: () => import("../views/My/template.vue"),
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    //console.log(store.getters.isLogin);
+    store
+      .dispatch("checkLogin")
+      .then(next())
+      .catch((err) => {
+        if (err.status === 'fail') {
+          next({
+            path: "/login",
+            query: { redirect: to.fullPath },
+          });
+        } else {
+          next();
+        }
+      });
+  } else {
+    next();
+  }
+});
+
+export default router;
---
+++ src/views/Detail/template.js
@@ -0,0 +1,35 @@
+import { marked } from "marked";
+import blog from "@/api/blog.js";
+
+export default {
+  data() {
+    return {
+      title: "",
+      rawContent: "",
+      user: {},
+      createdAt: "",
+    };
+  },
+
+  created() {
+    this.blogId = this.$route.params.blogId;
+    blog
+      .getDetail({ blogId: this.blogId })
+      .then((res) => {
+        console.log(res);
+        this.title = res.data.title;
+        this.rawContent = res.data.content;
+        this.createdAt = res.data.createdAt;
+        this.user = res.data.user;
+      })
+      .catch(res => {
+        console.log(res)
+      })
+  },
+
+  computed: {
+    markdown() {
+      return marked(this.rawContent);
+    },
+  },
+};
